e70f32afb182f62b8ea163a64194fb0072447e91
Alejandra <alejita.9813.dg@gmail.com>
2018-09-20 13:51:23 -0500

    commit inicial

---
+++ src/main/java/edu/eci/arsw/spacefight/spacefight/SpacefightApplication.java
@@ -0,0 +1,12 @@
+package edu.eci.arsw.spacefight.spacefight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacefightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacefightApplication.class, args);
+	}
+}
